7f82f028e699972051c5a75a19da765fc759e77d
otolithic <otolithic@users.noreply.github.com>
2018-10-14 11:44:22 -0400

    Initialize repo

---
+++ assets.js
@@ -0,0 +1,105 @@
+//images
+
+//bodies
+//form the basis of a new monster
+//their attachments define the location and orientation of appendages that can be added (x,y,anglemin,anglemax)
+var kitty = new Image()
+kitty.type = "body";
+kitty.src = "Images/bodies/kitty.png"
+kitty.offsetX = 50
+kitty.width = 300
+kitty.height=300
+kitty.hasAttachments = true
+kitty.attachments = [
+    [230,200,0,10],//left leg
+    [270,200,350,360],//right leg
+    [250,95,240,300],//head
+    [108,270,45,135]]//tail
+
+var armadillo = new Image()
+armadillo.type = "body"
+armadillo.src="Images/bodies/armadillo.png"
+armadillo.width = 500
+armadillo.height = 275
+armadillo.hasAttachments = true
+armadillo.attachments = [
+    [60,210,50,70], //front leg
+    [420,230,-60,60], //back leg
+    [35,120,75,130]] //head
+
+var giraffe = new Image()
+giraffe.type="body"
+giraffe.src = "Images/bodies/giraffe.png"
+giraffe.width=309
+giraffe.height=376
+giraffe.hasAttachments=true
+giraffe.attachments = [
+    [19,25,180,240],//head
+    [84,295,10,10],//front leg
+    [209,295,10,30],//back leg
+    [220,295,10,10]//other back leg
+]
+
+//features
+//attached within the body of the monster
+var eye1 = new Image()
+eye1.type = "feature"
+eye1.src = "Images/features/eye1.png"
+
+var mouth = new Image()
+mouth.type = "feature"
+mouth.src="Images/features/mouth1.png"
+
+var valves = new Image()
+valves.type = "feature"
+valves.src="Images/features/valves.png"
+
+var suriname = new Image()
+suriname.type="feature"
+suriname.src="Images/features/suriname.png"
+
+//appendages
+//attached along the boundary of the monster
+var anemone = new Image()
+anemone.type = "appendage"
+anemone.src="Images/appendages/anemone.png"
+anemone.attachAt = [144,4]
+
+var anemone2 = new Image()
+anemone2.type = "appendage"
+anemone2.src="Images/appendages/anemone2.png"
+anemone2.attachAt = [55,10]
+
+var anemone3 = new Image()
+anemone3.type = "appendage"
+anemone3.src="Images/appendages/anemone3.png"
+anemone3.width=300
+anemone3.attachAt = [40,54]
+
+var coral1 = new Image()
+coral1.type = "appendage"
+coral1.src="Images/appendages/coral1.png"
+coral1.width = 150
+coral1.height=150
+coral1.attachAt = [15,65]
+
+var hand1 = new Image()
+hand1.type="appendage"
+hand1.src="Images/appendages/hand1.png"
+hand1.attachAt=[14,11]
+
+//textures (backgrounds, perhaps skins)
+var decopaper4 = new Image()
+decopaper4.src="Images/textures/decopaper4.jpg"
+
+var decopaper7 = new Image()
+decopaper7.src="Images/textures/decopaper7.jpg"
+
+var decopaper13 = new Image()
+decopaper13.src="Images/textures/decopaper13.jpg"
+
+//data structures
+var bodies = [kitty, armadillo, giraffe]
+var features = [eye1, mouth,valves,suriname]
+var appendages = [anemone, anemone2, anemone3,hand1]
+var textures = [decopaper4,decopaper7]
---
+++ main.js
@@ -0,0 +1,164 @@
+//define the canvas and attach it
+var canvas = document.createElement("canvas")
+document.body.appendChild(canvas)
+canvas.width=1000
+canvas.height=630
+//center the canvas on the page
+var marginLeft = document.body.getBoundingClientRect().width/2 - canvas.width/2
+canvas.style.marginLeft=`${marginLeft}px`
+
+//set the context to 2D
+var context = canvas.getContext("2d")
+
+allAssets = [...bodies, ...features, ...appendages, ...textures]
+var promise = new Promise(function(resolve, reject){
+    //load the images
+    console.log("loaded!")
+    var numloaded = 0
+    for(let asset of allAssets){
+         asset.addEventListener('load', function() {
+             numloaded++
+             console.log(numloaded, asset)
+             if (numloaded == allAssets.length){
+                 resolve()
+             }
+         })
+    }
+        
+    console.log("out of body")
+})
+promise.then(function(result){
+    render()
+    console.log("result", result)
+})
+
+function render(){
+    console.log("in render")
+    var bg = decopaper4
+    context.drawImage(bg,0,0)
+//generate which parts to use
+
+var bodynumber = Math.round(Math.random()*(bodies.length-1))
+
+numlimbs = bodies[bodynumber].attachments.length
+maxfeats = 3
+numfeatures = Math.round(Math.random()*maxfeats)
+
+limblist = getIdxList(numlimbs,appendages)
+featlist = getIdxList(numfeatures,features)
+
+//put the parts in an array with the body first
+var parts = [bodies[bodynumber]]
+
+for (var i = 0; i < featlist.length; i++){
+    parts = parts.concat(featlist[i])
+}
+for (var i = 0; i < limblist.length; i++){
+    parts = parts.concat(limblist[i])
+}
+
+//iterate and display the parts
+jointnum = 0;
+for (var i = 0; i < parts.length; i++){
+    img = parts[i]
+    //instantiate body position
+    if (i == 0) {
+        xref = canvas.width/2-img.width/2
+        yref = canvas.height/2-img.height/2
+        xloc = xref
+        yloc = yref
+        angle = 0
+        axisX = img.width/2
+        axisY = img.width/2
+    }
+    
+    //select feature placement
+    else {
+        if (parts[0].hasAttachments && img.type == "appendage"){
+            jointnum++
+            if (jointnum>=parts[0].attachments.length)
+                jointnum = 0
+            joint = parts[0].attachments[jointnum]
+            
+            xloc = xref + joint[0] - img.attachAt[0]
+            yloc = yref + joint[1] - img.attachAt[1]
+            
+            angle = Math.random()*(joint[3]-joint[2])+joint[2]
+            axisX = img.attachAt[0]
+            axisY = img.attachAt[1]
+        }
+        else {
+            //randomize x and y coords based on image size
+            xloc = Math.round(Math.random()*(parts[0].width-img.width))+canvas.width/2-parts[0].width/2
+            yloc = Math.round(Math.random()*(parts[0].height-img.height))+canvas.height/2-parts[0].height/2
+            angle = Math.random()*180-90
+            axisX = img.width/2
+            axisY = img.width/2
+        }
+    }    
+    
+    drawPart(img, angle*Math.PI/180, xloc, yloc)
+    console.log(context.globalCompositeOperation)
+        
+//    //uncomment to check joint locations
+//    
+//    if (img.hasAttachments){
+//        for (var a = 0; a < img.attachments.length; a++){
+//            joint = img.attachments[a]
+//            rectx = xloc + joint[0]
+//            recty = yloc + joint[1]
+//            context.fillStyle="red"
+//            context.fillRect(rectx,recty,10,10)
+//        }
+//    }
+//    
+//    
+}
+
+
+function drawPart(img, angle, targetPointX,targetPointY)
+{
+    console.log("in drawPart")
+    //puts the image at the given point
+    
+        if (img.type=="appendage"){
+            atX = img.attachAt[0]
+            atY = img.attachAt[1]
+            w = img.width
+            h = img.height
+        }
+        else if (img.type=="feature"){
+            atX = 0;
+            atY = 0;
+            r = Math.random()*0.5+0.5
+            w = img.width*r
+            h = img.height*r
+        }
+        else {
+            atX = 0;
+            atY = 0;
+            w = img.width
+            h = img.height
+        }
+       context.translate(targetPointX+atX,targetPointY+atY)
+        context.rotate(angle)
+        console.log(img)
+        console.log(img.type)
+        console.log(context.globalCompositeOperation)
+        context.drawImage(img, -atX,-atY,w,h)
+        
+        context.setTransform(1, 0, 0, 1, 0, 0);
+    
+}
+
+function getIdxList(num,array){
+    var idxlist = []
+
+    for (var i = 0; i < num; i++){
+        idx = Math.floor(Math.random()*array.length)
+        idxlist = idxlist.concat(array[idx])
+    }
+    
+    return idxlist
+}
+}//end render fxn
